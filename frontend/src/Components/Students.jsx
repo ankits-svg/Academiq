2f3243e1589073b773610b996f920dbbf42b725a
Ankit Sharma <ankits745@gmail.com>
2024-03-03 15:47:39 +0530

    pulling data from github

--- frontend/src/Components/Students.jsx
+++ frontend/src/Components/Students.jsx
@@ -10,16 +10,24 @@ const Students = () => {
         const obj={
             name,gender,age,skill
         }
+        
         console.log("obj:",obj)
+        alert("Thanks for submitting your info")
+        setName("")
+        setGender("")
+        setAge("")
+        setSkill("")
     }
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:"column", width:"50%",margin:"auto"}}>
       Name:<input placeholder='Your name' value={name} onChange={(e)=>setName(e.target.value)}/>
       Gender:<input placeholder='Your gender' value={gender} onChange={(e)=>setGender(e.target.value)}/>
       DOB:<input placeholder='Your age' value={age} onChange={(e)=>setAge(e.target.value)}/>
       Skills:<input placeholder='Your Skill' value={skill} onChange={(e)=>setSkill(e.target.value)}/>
 
+      <div>
       <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   )
 }
